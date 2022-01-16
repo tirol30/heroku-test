2fb8de3cb38f11cb2628df8f357b11230fb05b0c
tirol30 <74885035+tirol30@users.noreply.github.com>
2022-01-16 21:40:09 +0900

    Update app.js

--- web/deckgl/app.js
+++ web/deckgl/app.js
@@ -1,4 +1,5 @@
 new deck.DeckGL({
+  const DATA_URL = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json'
   mapStyle: 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json',
   initialViewState: {
     longitude: -122.45,
@@ -7,12 +8,18 @@ new deck.DeckGL({
   },
   controller: true,
   layers: [
-    new deck.ScatterplotLayer({
-      data: [
-        {position: [-122.45, 37.8], color: [255, 0, 0], radius: 100}
-      ],
-      getColor: d => d.color,
-      getRadius: d => d.radius
+    new GeoJsonLayer({
+      id: 'geojson',
+      DATA_URL,
+      opacity: 0.8,
+      stroked: false,
+      filled: true,
+      extruded: true,
+      wireframe: true,
+      getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
+      getFillColor: f => COLOR_SCALE(f.properties.growth),
+      getLineColor: [255, 255, 255],
+      pickable: true
     })
   ]
 });
